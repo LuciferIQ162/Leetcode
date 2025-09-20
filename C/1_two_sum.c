41779b8bd78b3aaca829925554c49bac762c7470
LuciferIQ162 <devil.com444@gmail.com>
2025-09-20 11:33:57 +0530

    leetcode 1:

--- C/1_two_sum.c
+++ C/1_two_sum.c
@@ -5,7 +5,7 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 {
     *returnSize = 2;
     int *result = (int*)malloc(2 * sizeof(int));
-    for(int i = 0; i < numSize - 1; i++)
+    for(int i = 0; i < numsSize - 1; i++)
         {
             for(int j = i + 1; j < numsSize; j++)
             {
@@ -20,4 +20,33 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
         }
         *returnSize = 0;
         return NULL;
+}
+//hashmap implementation here
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    struct hashTable {
+        int key;
+        int value;
+        UT_hash_handle hh;
+    } *hashTable = NULL, *item;
+
+    for (int i = 0; i < numsSize; i++) {
+        int complement = target - nums[i];
+        HASH_FIND_INT(hashTable, &complement, item);
+        if (item) {
+            int* result = malloc(sizeof(int) * 2);
+            result[0] = item->value;
+            result[1] = i;
+            *returnSize = 2;
+            HASH_CLEAR(hh, hashTable);  // Free the hash table
+            return result;
+        }
+        item = malloc(sizeof(struct hashTable));
+        item->key = nums[i];
+        item->value = i;
+        HASH_ADD_INT(hashTable, key, item);
+    }
+    *returnSize = 0;
+    HASH_CLEAR(hh, hashTable);  // Free the hash table
+    // Return an empty array if no solution is found
+    return malloc(0);  // Allocate 0 bytes
 }
